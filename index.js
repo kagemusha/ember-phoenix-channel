87a3d0301253f2fa1a750fc58ce9622a74e44558
kagemusha <mmadrid99@gmail.com>
2016-04-03 23:31:02 -0400

    WIP

--- index.js
+++ index.js
@@ -2,5 +2,28 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-phoenix-channel'
+  name: 'ember-phoenix-channel',
+
+  init: function(name) {
+    var assets_path = require('path').join('phoenix-socket','dist','socket.js');
+    this.treePaths['vendor'] = require.resolve('phoenix-socket').replace(assets_path, '');
+  },
+  //
+  //included: function(app) {
+  //  var options = this.app.options.phoenixChannelOptions || {enabled: true};
+  //
+  //  if (options.enabled) {
+  //    this.app.import('vendor/ic-ajax/dist/named-amd/main.js', {
+  //      exports: {
+  //        'ic-ajax': [
+  //          'default',
+  //          'defineFixture',
+  //          'lookupFixture',
+  //          'raw',
+  //          'request',
+  //        ]
+  //      }
+  //    });
+  //  }
+  //}
 };
